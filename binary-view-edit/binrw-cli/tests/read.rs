cff6bcb56720409fca13d002cb18b349272797ef
jackalope-code <22228272+jackalope-code@users.noreply.github.com>
2024-12-19 14:13:39 -0700

    Add stubs for read tests

---
+++ binary-view-edit/binrw-cli/tests/read.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod read_tests {
+    use super::*;
+
+    #[test]
+    fn read_large_positive_offsets() {
+        assert_eq!(4, 4);
+    }
+    
+    #[test]
+    fn read_large_positive_offset_to_negative_offset() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn read_negative_offset_to_large_positive_offsets() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn read_large_positive_offset_to_eof() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn read_negative_offset_to_eof() {
+        assert_eq!(4, 4);
+    }
+    
+    #[test]
+    fn read_negative_to_negative_ascending_pass() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn read_negative_to_negative_descending_fail() {
+        assert_eq!(4, 4);
+    }
+}
