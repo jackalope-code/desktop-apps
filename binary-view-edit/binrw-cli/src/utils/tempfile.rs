5a8b900150b74ac2462f4017f0eece2f17a79438
JackalopeCode <22228272+jackalope-code@users.noreply.github.com>
2024-12-31 09:53:14 -0700

    Remove utils module namespace

--- binary-view-edit/binrw-cli/src/utils/tempfile.rs
+++ binary-view-edit/binrw-cli/src/utils/tempfile.rs
@@ -5,49 +5,46 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::io;
 
-// pub mod utils {
+pub struct TempFile {
+  path_ref: PathBuf,
+  file: Option<File>,
+  keep_file: bool
+}
 
-  pub struct TempFile {
-    path_ref: PathBuf,
-    file: Option<File>,
-    keep_file: bool
+impl TempFile {
+  pub fn new(filename: &str, keep_file: bool) -> io::Result<Self> {
+      let mut test_output_file_path = PathBuf::new();
+      test_output_file_path.push(filename);
+
+      // Create new empty temp file.
+      let file = File::create(test_output_file_path.as_path()).expect("Could not create temp file");
+
+      Ok(TempFile {
+          path_ref: test_output_file_path,
+          file: Some(file),
+          keep_file
+      })
   }
-  
-  impl TempFile {
-    pub fn new(filename: &str, keep_file: bool) -> io::Result<Self> {
-        let mut test_output_file_path = PathBuf::new();
-        test_output_file_path.push(filename);
-  
-        // Create new empty temp file.
-        let file = File::create(test_output_file_path.as_path()).expect("Could not create temp file");
-  
-        Ok(TempFile {
-            path_ref: test_output_file_path,
-            file: Some(file),
-            keep_file
-        })
-    }
-  
-    pub fn as_file(&mut self) -> Option<&mut File> {
-        self.file.as_mut()
-    }
-  
-    pub fn path_str(&mut self) -> &str {
-        return self.path_ref.to_str().unwrap()
-    }
-  
-    // fn path(&mut self) -> &Path {
-    //     return self.path_ref
-    // }
+
+  pub fn as_file(&mut self) -> Option<&mut File> {
+      self.file.as_mut()
   }
-  
-  impl Drop for TempFile {
-    fn drop(&mut self) {
-        println!("TempFile dropping out of scope!!!");
-        if !self.keep_file {
-            println!("Deleting file!!!");
-            fs::remove_file(self.path_ref.as_path());
-        }
-    }
+
+  pub fn path_str(&mut self) -> &str {
+      return self.path_ref.to_str().unwrap()
   }
-// }
+
+  // fn path(&mut self) -> &Path {
+  //     return self.path_ref
+  // }
+}
+
+impl Drop for TempFile {
+  fn drop(&mut self) {
+      println!("TempFile dropping out of scope!!!");
+      if !self.keep_file {
+          println!("Deleting file!!!");
+          fs::remove_file(self.path_ref.as_path());
+      }
+  }
+}
