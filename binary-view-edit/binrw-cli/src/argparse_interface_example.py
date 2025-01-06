9fdcdb13019f406f2454c940a8b0ee72007f03ef
JackalopeCode <22228272+jackalope-code@users.noreply.github.com>
2025-01-05 21:29:15 -0700

    Add read/write and splice/overwrite options to mock CLI

--- binary-view-edit/binrw-cli/src/argparse_interface_example.py
+++ binary-view-edit/binrw-cli/src/argparse_interface_example.py
@@ -32,9 +32,32 @@ main_command_group = parser.add_mutually_exclusive_group(required=True)
 
 # binrw file write overwrite (--pad-zeroes start/end | --write_exact (default)) (input_position) (input_end_position) (input_file) (output_end_position) (output_size) (output_file) 
 
+# python src/argparse_interface_example.py --file hello --read --position 0 0
+# python src/argparse_interface_example.py --file hello write --splice --position 0 0
 main_command_group.add_argument('--file')
 main_command_group.add_argument('--metadata')
 main_command_group.add_argument('--diff')
+
+file_option_subparser = parser.add_subparsers(help="File options")
+read_parser = file_option_subparser.add_parser('--read')
+read_offset_group = read_parser.add_mutually_exclusive_group(required=True)
+read_offset_group.add_argument('--position', nargs=2, metavar='offset')
+read_offset_group.add_argument('--amount', nargs=2)
+
+write_parser = file_option_subparser.add_parser('write')
+
+write_mode_group = write_parser.add_mutually_exclusive_group(required=True)
+# TODO: Ideally store in one var with a String enum for flags.
+write_mode_group.add_argument('--splice', action='store_true')
+write_mode_group.add_argument('--overwrite', action='store_true')
+
+write_offset_group = write_parser.add_mutually_exclusive_group(required=True)
+write_offset_group.add_argument('--position', nargs=2, metavar='offset')
+write_offset_group.add_argument('--amount', nargs=2)
+
+# write_option_subparser = write_parser.add_subparsers(help="Write options")
+# write_splice = write_option_subparser.add_parser("--splice")
+# write_overwrite = write_option_subparser.add_parser("--overwrite")
 
 args = parser.parse_args()
 print(args)
